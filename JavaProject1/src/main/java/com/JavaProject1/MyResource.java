15974eef48b978b880b6370316555fc1f803df2e
susman <rinku268@gmail.com>
2018-02-08 10:07:33 +0530

    Add resource
    Change-Id: I0a8c442b3988a4c4005677b087f81a9d110cbfee
    Signed-off-by: susman <rinku268@gmail.com>

---
+++ JavaProject1/src/main/java/com/JavaProject1/MyResource.java
@@ -0,0 +1,30 @@
+package com.JavaProject1;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.bo.Employee;
+import com.dao.EmployeeDao;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Employee> getIt() {
+        return new EmployeeDao().getAllEmployee();
+    }
+}
