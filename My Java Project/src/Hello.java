266b1e7a9f63f2466180057e26edb4d12febb2d9
Susmanmaharana <rinku268@gmail.com>
2018-02-08 12:29:34 +0530

    Raja branch of dev1 is commited

--- My Java Project/src/Hello.java
+++ My Java Project/src/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("susman maharana");
+		System.out.println("rinku maharana");
 	}
 }
