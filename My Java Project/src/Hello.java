9622e78c18e03512436aa35742a48664cfa7d4c0
Susmanmaharana <rinku268@gmail.com>
2018-02-08 16:16:14 +0530

    5th and 6th statemnet push to remote Repository

--- My Java Project/src/Hello.java
+++ My Java Project/src/Hello.java
@@ -2,10 +2,12 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		int a=10,b=38;
 		System.out.println("hello");
 		System.out.println("susman maharana");
 		System.out.println("rinku maharana");
 		System.out.println("arpita maharana");
-		
+		System.out.println("Sum of A and B is "+(a+b));
+		System.out.println("Narendra Modi is a great person in world");
 	}
 }
