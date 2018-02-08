d61149a4330e1240fcb0c9b215f8a6f794011259
Susmanmaharana <rinku268@gmail.com>
2018-02-08 11:59:26 +0530

    new project commit

--- ff/src/main/java/com/ff/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.ff;
-
-public class Employee {
-	public static void main(String[] args) {
-		String str1 = "susman";
-		String str2 = str1.intern();
-		System.out.println(str1 + "    " + str2);
-	}
-}
